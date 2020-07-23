6c234c346cbcb540796859254b52b97d0e9d6fcb
zachung <zach50931@gmail.com>
2020-07-23 17:36:51 +0800

    ele war

---
+++ src/single/ele-war/src/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div id="world" :style="worldStyle">
+      <template v-for="row in world">
+        <div v-for="cell in row" class="tile">
+          {{ cell }}
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+const empty = '\u25A2'
+const wall = '\u25A6'
+const N = 100
+const world = []
+for (let r = 0; r < N; r++) {
+  world[r] = []
+  for (let c = 0; c < N; c++) {
+    world[r][c] = empty
+  }
+}
+
+world[20][20] = wall
+
+export default {
+  data() {
+    return {
+      world,
+      tileCount: N
+    }
+  },
+  computed: {
+    worldStyle() {
+      return {
+        display: 'grid',
+        'grid-template-rows': 'repeat(' + N + ', 1em)',
+        'grid-template-columns': 'repeat(' + N + ', 1em)'
+      }
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<style scoped>
+.tile {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1em;
+}
+</style>
---
+++ src/single/ele-war/src/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Index from './Index.vue'
+
+new Vue({
+  el: '#app',
+  components: { Index },
+  template: '<index/>'
+})
